e03b34ac43d717ef356d54677b3cf305b17a02c9
spookyd13 <lpdavis13@gmail.com>
2017-01-22 01:21:35 -0500

    Refactored client and added status display

--- speed.js
+++ speed.js
@@ -2,31 +2,50 @@
  * Created by lukedavis on 1/20/17.
  */
 
-var speedTest = require('speedtest-net');
-var speed = speedTest({maxTime: 5000});
+const speedTest = require('speedtest-net');
+const speed = speedTest({maxTime: 5000});
 
-module.exports = function (RED) {
-	console.log("Setting up node");
-	function FastNode(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
-		this.on('input', function (msg) {
-			speed.on('downloadprogress', progress => {
-				console.log('Download progress:', progress);
-			});
+var SpeedNode = function (RED) {
+	var node = this;
+	this.msg = {};
+	RED.nodes.registerType("speed-test", function (configuration) {
+		node.config = configuration;
+		RED.nodes.createNode(node, node.config);
+		node.on('input', node.registerListeners(node));
+	});
 
-			speed.on('data', function (data) {
-				console.log("Got that data " + JSON.stringify(data));
-				msg.payload = data;
-				node.status({fill: "green", shape: "ring", text: data.speeds.download + " Mbps"});
-				node.send(msg);
-			});
-
-			speed.on('error', function (error) {
-				console.error(error);
-			});
-		});
-	}
-
-	RED.nodes.registerType("speed-speed", FastNode);
 };
+
+SpeedNode.config = {};
+SpeedNode.msg = {};
+
+SpeedNode.prototype.registerListeners = function (node) {
+	return function (msg) {
+		node.msg = msg;
+		speed.on('downloadprogress', node.updateProgress(node));
+		speed.on('data', node.handleCompletion(node));
+		speed.on('error', node.handleError(node));
+	}
+};
+
+SpeedNode.prototype.updateProgress = function (node) {
+	return function (progress) {
+		node.status({fill: "yellow", shape: "dot", text: progress + " %"});
+	}
+};
+
+SpeedNode.prototype.handleCompletion = function (node) {
+	return function (data) {
+		node.msg.payload = data;
+		node.status({fill: "green", shape: "dot", text: data.speeds.download + " Mbps"});
+		node.send(node.msg);
+	}
+};
+
+SpeedNode.prototype.handleError = function (node) {
+	return function (error) {
+		node.error('Speedtest error: ' + error)
+	}
+};
+
+module.exports = SpeedNode;
--- speed.test.js
+++ speed.test.js
@@ -1,50 +1,38 @@
 'use strict';
 
 var expect = require('chai').expect;
+var mock = require('sinon');
 var client = require('./speed');
 
-describe('client', function() {
-	it('should get callbacks', function() {
-		client(1000, function (result) {
-			expect(result).to.equal(result);
-		});
-	});
+var MOCK_RED = {
+	myNode: {},
+	myConfig: {},
+	callback: function() {},
+	sent: {
+		msg: {},
+		status: {},
+		config: {},
+	},
+	nodes: {
+		createNode: function (t, c) {
+			t.status = function (a) {
+				this.sentMsg = a;
+			};
+			t.on = function (n, cb) {
+				console.log("Setting callback for " + n);
+				this.callback = cb;
+			};
+			t.send = function (m) {
+				console.log("Yeah bitch");
+			}
+		},
+		registerType: function (name, node) {
+			this.myNode = node;
+			this.myNode();
+		}
+	},
+};
 
-	it('should get progress updates', function() {
-		client(1000, null, function (result) {
-			expect(result).to.equal(result);
-		});
-	});
+describe('speed tests', function () {
+
 });
-
-// MOCK
-// var callback = function() {};
-//
-// async function run() {
-// 	var RED = {
-// 		myNode: {},
-// 		myConfig: {},
-// 		nodes: {
-// 			createNode: function (t, c) {
-// 				t.status = function(a) {
-// 					console.log("Got status " + a);
-// 				};
-// 				t.on = function(n, cb) {
-// 					console.log("Setting callback for " + n);
-// 					callback = cb;
-// 				};
-// 				t.send = function(m) {
-// 					console.log("Yeah bitch");
-// 				}
-// 			},
-// 			registerType: function (name, node) {
-// 				this.myNode = node;
-// 				this.myNode();
-// 			}
-// 		},
-// 	};
-// 	s(RED);
-// }
-//
-// run();
-// callback({});
